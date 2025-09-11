24abce992fc889a5b4a0b1929c5a2e5340833e5d
rdgd <ryan.is.gray@gmail.com>
2025-09-11 15:40:50 -0500

    update

---
+++ setup-env.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+cd $HOME 
+mkdir -p $HOME/.config/
+
+#MISE
+if type -p mise >/dev/null; then
+  echo "Mise already installed... skipping"
+else
+  echo "Installing mise..."
+  curl https://mise.run | sh
+  echo "eval \"\$($HOME/.local/bin/mise activate zsh│
+  )\"" >> "$HOME/.zshrc"
+fi
+
+git clone git@github.com:rdgd/mise-config.git $HOME/.config/mise
+cd $HOME/.config/mise && mise install
+cd $HOME
+
+git clone git@github.com:rdgd/nvim-config.git
+cd $HOME
+
+#GITCONFIG
+git config --global user.email "ryan.is.gray@gmail.com"
+git config --global user.name "rdgd"
+
+#DOTFILES
+git clone git@github.com:rdgd/dotfiles.git $HOME/dotfiles
+cp $HOME/dotfiles/.vimrc .
+cp $HOME/dotfiles/.tmux.conf .
+cp $HOME/dotfiles/.zshrc .
+
+
+
+
+
+
+
